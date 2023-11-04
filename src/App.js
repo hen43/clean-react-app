9a2ce4f3528cc95c48daf17189670776e97dbcd0
qwer9iqwe0r <whshle08@gmail.com>
2023-11-03 17:00:30 -0700

    amogus

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import CyberLions from "./assets/cyberlions.jpg";
-
+import Compress from"./components/Compress"
+import React from 'react'
 
 function App() {
   return (
-    <div className="app">
-      <section className="theThing">
-        <img src={CyberLions} alt="le cyberli'on"></img>
-        <div className="text">
-          <h1>We're a High School Robotics Team Based In Westminster CA</h1>
-          <div class="literallyJustALine"></div>
-          <p>Founded in 2020, Team 8521 is a robotics team aiming to provide our community with hands-on opportunities and mentorships. We want to empower our community and give them the chance to show their leader and innovator ability.</p>
-        </div>
-      </section>
-    </div>
+      <div>
+        <Compress />
+        <Compress />
+        <Compress />
+      </div>
   );
 }
 
