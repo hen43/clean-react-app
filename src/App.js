66324b975ab3e31335ffa98610a82fdc4db8656f
Henry Le <140691753+hen43@users.noreply.github.com>
2023-11-17 17:05:52 -0800

    i did the balogna
    garfield

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import Compress from"./components/Compress"
 import React from 'react'
+
+import Kevilry from './assets/kevilry.jpg'
 
 function App() {
   return (
-      <div>
-        <Compress />
-        <Compress />
-        <Compress />
+      <div className="app">
+        <div className="thingy">
+          <img src={Kevilry} className="robot" alt="kevilry"></img>
+          <div className="text">
+            <h1>WSG MY BRO</h1>
+            <p>FRC is love, FRC is life. Joe mama!!! Something... filler text... Lorem ipsum something else blehh... joe !!! xdddd something somethung asidjsaiodjsaiodj ioasjd osaasioj sad</p>
+          </div>
+        </div>
       </div>
   );
 }
